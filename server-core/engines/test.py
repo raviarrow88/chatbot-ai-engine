9149f7217e67335df8f0b9c0890d84c150795e9a
Ravi Merugu <ravi.merugu@invaana.com>
2016-12-14 00:38:28 +0530

    correction in log file name

--- server-core/engines/test.py
+++ server-core/engines/test.py
@@ -8,5 +8,5 @@ from scraper import scrape_clean_html
 
 html_data = scrape_clean_html("http://manual.gromacs.org/documentation/2016.1/user-guide/getting-started.html#flowchart-of-typical-simulation")
 # print html_data
-with open("html_data.txt",'w') as fh:
+with open("html_data.log",'w') as fh:
     fh.write(html_data)
