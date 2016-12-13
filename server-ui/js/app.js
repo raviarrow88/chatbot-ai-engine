71f34c78b9aec34e55375fbf069abd8dcaf20596
Ravi Teja <raviarrow88@gmail.com>
2016-12-13 10:44:26 +0530

    Changed AppName in index and js File

--- server-ui/js/app.js
+++ server-ui/js/app.js
@@ -1,8 +1,8 @@
 	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	var chatBotApp = angular.module('chatBotApp', ['ngRoute']);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	chatBotApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
@@ -21,11 +21,11 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('baseController', function($scope) {
+	chatBotApp.controller('baseController', function($scope) {
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
 	});
 
-	scotchApp.controller('talkController', function($scope) {
+	chatBotApp.controller('talkController', function($scope) {
 		$scope.message = 'Look! I am an about page.';
 	});
